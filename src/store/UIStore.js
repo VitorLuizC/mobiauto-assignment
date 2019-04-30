575c8b65115b05b0abdf6eb319574974fa9f2813
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-04-30 10:48:32 -0300

    :lipstick: use consistent code style on modules

--- src/store/UIStore.js
+++ src/store/UIStore.js
@@ -1,30 +1,30 @@
-import { action, observable, decorate } from 'mobx'
-import remotedev from 'mobx-remotedev'
+import { action, observable, decorate } from "mobx";
+import remotedev from "mobx-remotedev";
 
 const _defaultInitialState = {
-  isFetching: false,
-}
+  isFetching: false
+};
 
 class UIStore {
   constructor(initialState) {
-    this.setInitialState(initialState || _defaultInitialState)
+    this.setInitialState(initialState || _defaultInitialState);
   }
 
   setInitialState = initialState => {
-    const { isFetching } = initialState
+    const { isFetching } = initialState;
 
-    this.isFetching = isFetching
-  }
+    this.isFetching = isFetching;
+  };
 
   setIsFetching = isFetching => {
-    this.isFetching = isFetching
-  }
+    this.isFetching = isFetching;
+  };
 }
 
 export default remotedev(
   decorate(UIStore, {
     isFetching: observable,
     setInitialState: action,
-    setIsFetching: action,
+    setIsFetching: action
   })
-)
+);
