2b25df4da19a41e38736c94a5d1ede5f24cb43e5
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-04-30 10:45:39 -0300

    :fire: remove react-router-dom and its routes configuration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import { Provider } from "mobx-react";
 import store from "../src/store";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
-import routes from "../src/config/routes";
-class App extends Component {
-  render() {
-    return (
-      <Provider {...store}>
-        <BrowserRouter>
-          <Switch>
-            {routes.map(route => (
-              <Route {...route} />
-            ))}
-          </Switch>
-        </BrowserRouter>
-      </Provider>
-    );
-  }
+import HomeContainer from "./containers/Home";
+
+function App() {
+  return (
+    <Provider {...store}>
+      <HomeContainer />
+    </Provider>
+  );
 }
 
 export default App;
