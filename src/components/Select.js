a258f7a2c78c913d5f6195e147bbe7836857eae2
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-05-02 12:12:51 -0300

    :sparkles: improve Select component and its style

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,29 +1,79 @@
-import uuid from "uuid/v4";
-import React from "react";
+import "./Select.css";
+import React, { useState } from "react";
+import Modal from "./Modal";
+import CaretDown from "../assets/icons/Caret-Down.svg";
 
-function SelectOption({ text, value }) {
-  return (
-    <option key={value} value={value}>
-      {text}
-    </option>
-  );
+/**
+ * @typedef Option
+ * @property {string} text
+ * @property {string} value
+ */
+
+/**
+ * Creates a match function to compare option with term.
+ * @param {string} term - A search term to be compared.
+ * @returns {(option:Option) => boolean}
+ */
+function optionMatchesTerm(term) {
+  return option => option.text.toLowerCase().includes(term.toLocaleLowerCase());
 }
 
 function Select({ label, options, value, onChange, placeholder }) {
-  const id = "select-" + uuid();
+  const [term, setTerm] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const option = options.find(option => option.value === value);
+
+  const onClose = () => {
+    setTerm("");
+    setIsOpen(false);
+  };
 
   return (
-    <fieldset>
-      <label htmlFor={id}>{label}</label>
-      <select
-        id={id}
-        value={value}
-        onChange={event => onChange(event.target.value)}
-      >
-        <option disabled>{placeholder}</option>
-        {options.map(SelectOption)}
-      </select>
-    </fieldset>
+    <div className="Select">
+      <p className="label">{label}</p>
+      <div className="field" onClick={() => setIsOpen(true)}>
+        {option ? (
+          <span className="value">{option.text}</span>
+        ) : (
+          <span className="placeholder">{placeholder}</span>
+        )}
+
+        <img className="caret" src={CaretDown} alt="Abrir" title="Abrir" />
+      </div>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <div className="Select-modal-content">
+          <div className="filter">
+            <input
+              type="text"
+              value={term}
+              onChange={event => setTerm(event.target.value)}
+              className="input"
+            />
+          </div>
+
+          <ul className="options">
+            {options.filter(optionMatchesTerm(term)).map(({ text, value }) => {
+              const isActive = option && option.value === value;
+
+              return (
+                <li
+                  key={value}
+                  onClick={() => {
+                    onClose();
+                    onChange(value);
+                  }}
+                  className={`option${isActive ? " -active" : ""}`}
+                >
+                  {text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </Modal>
+    </div>
   );
 }
 
